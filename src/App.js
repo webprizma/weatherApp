3e5d565b10f66cfca529c1fbc0ab58c29a031e91
Yuri Kulagin <54793523+webprizma@users.noreply.github.com>
2022-11-20 04:58:38 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class WeatherDisplay extends Component {
   }
   componentDidMount() {
     const zip = this.props.zip;
-    const URL = "http://api.openweathermap.org/data/2.5/weather?q=" +
+    const URL = "https://api.openweathermap.org/data/2.5/weather?q=" +
         zip +
         "&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=imperial";
     fetch(URL).then(res => res.json()).then(json => {
